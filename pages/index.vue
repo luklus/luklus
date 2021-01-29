4948e0ba9bbfa5f027ef9b78753670075cc0c2ab
luklus <ll_code@outlook.com>
2021-01-29 12:40:32 +0100

    feat: clean

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,5 @@
 <template>
-  <main class="home">
-    <h1>Color mode: {{ $colorMode.value }}</h1>
-    <select v-model="$colorMode.preference">
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-    </select>
-  </main>
+  <main class="home"></main>
 </template>
 
 <script>
