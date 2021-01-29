6318fd27df738144719816549af0d38e1e249ca6
luklus <ll_code@outlook.com>
2021-01-29 14:13:53 +0100

    feat: add logo component

---
+++ components/apps/AppsLogo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="logo">
+    <nuxt-link to="/">
+      {{ $t('name') }}
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'AppsLogo',
+})
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  padding: 0.25rem;
+
+  a {
+    color: inherit;
+    font-size: 1.25rem;
+    text-decoration: none;
+  }
+}
+</style>
