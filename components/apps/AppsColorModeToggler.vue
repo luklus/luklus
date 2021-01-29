d19d5302a411e1bd080264ae9a11d75e512600a2
luklus <ll_code@outlook.com>
2021-01-29 14:14:23 +0100

    feat: add color mode toggler component

---
+++ components/apps/AppsColorModeToggler.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="apps-color-mode-toggler">
+    <moon-icon
+      v-if="$colorMode.preference === 'dark'"
+      @click="toogleColorMode('light')"
+    />
+    <sun-icon v-else @click="toogleColorMode('dark')" />
+  </div>
+</template>
+
+<script>
+import { defineComponent, useContext } from '@nuxtjs/composition-api'
+import MoonIcon from '@/assets/icons/moon.svg?inline'
+import SunIcon from '@/assets/icons/sun.svg?inline'
+
+export default defineComponent({
+  name: 'AppsColorModeToggler',
+
+  components: {
+    MoonIcon,
+    SunIcon,
+  },
+
+  setup() {
+    const { $colorMode } = useContext()
+
+    const toogleColorMode = (mode) => ($colorMode.preference = mode)
+
+    return { toogleColorMode }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.apps-color-mode-toggler {
+  align-items: center;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  padding: 0.25rem;
+}
+</style>
