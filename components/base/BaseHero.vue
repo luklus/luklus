2a64acfef3ecf74c7d5e121e9acb9d7fc22007b9
luklus <ll_code@outlook.com>
2021-01-29 22:59:30 +0100

    feat: add base hero

---
+++ components/base/BaseHero.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="base-hero">
+    <div class="wrap base-hero__wrap">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'BaseHero',
+})
+</script>
+
+<style lang="scss" scoped>
+.base-hero {
+  &__wrap {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    padding: 2rem;
+  }
+}
+</style>
