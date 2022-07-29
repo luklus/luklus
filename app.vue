d4665d43687f7adb139e2b9631cf5013443c9766
Lukas Lusiak <ll_code@outlook.com>
2022-07-29 14:43:54 +0200

    feat: add hero section

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <!-- <header>Header</header> -->
+  <main class="page">
+    <PartHero />
+    <section class="page__part">Hello</section>
+    <section class="page__part">Hello</section>
+  </main>
+  <!-- <footer>Footer</footer> -->
 </template>
+
+<style lang="scss" scoped>
+.page {
+  height: 100vh;
+  overflow: auto;
+  scroll-behavior: smooth;
+  scroll-snap-type: y mandatory;
+
+  &__part {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    margin: 0 auto;
+    max-width: 60rem;
+    padding: 1.25rem;
+    scroll-snap-align: start;
+    width: 100%;
+  }
+}
+</style>
---
+++ components/part/PartHero.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="part hero">
+    <div class="hero__mssg">
+      <p>cześć, jestem</p>
+    </div>
+    <h1>Łukasz Łusiak</h1>
+    <h2>Frontend Developer</h2>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.hero {
+  &__mssg {
+    background-color: #fbbf24;
+    border-radius: 1.25rem 1.25rem 1.25rem 0;
+    display: flex;
+    font-weight: 700;
+    justify-content: flex-start;
+    padding: 1rem;
+    width: 12rem;
+  }
+}
+</style>
